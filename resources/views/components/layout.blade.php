96d18974cf5bcb116b8f2748b449f04c4052d41f
david30-maker <davidokpah396@gmail.com>
2024-12-25 06:19:21 +0100

    working on dashboard

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -113,11 +113,10 @@
   </header>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
+      Slotted content goes here
     </div>
   </main>
 </div>
-
-    </body>
+</body>
 </html>
     
