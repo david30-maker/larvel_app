96ffa83113b8f76d67ab5022af63eceb150b98dc
david30-maker <davidokpah396@gmail.com>
2024-12-25 07:33:34 +0100

    add heading

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body class="font-sans antialiased">
         <!--
   This example requires updating your template:
 
@@ -108,12 +108,13 @@
 
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading ?? ''}}</h1>
     </div>
   </header>
+
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      Slotted content goes here
+      {{ $slot }}
     </div>
   </main>
 </div>
