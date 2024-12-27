6797d48c4ce5928e1fa60a0604c8bb7b3c12b393
david30-maker <davidokpah396@gmail.com>
2024-12-27 07:01:13 +0100

    add layout

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,9 @@
-@props(['active' => false ])
+@props(['active' => false])
 
-<a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-white" 
-    aria-current="{{ request()->is('/') ? 'page': 'false' }}">
-    {{ $attributes }}
+<a 
+    href="{{ $attributes->get('href') }}" 
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+>
     {{ $slot }}
 </a>
